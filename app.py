699211760fd1c1ec1712b21abe291c02236746f7
achieveradarsh <158289007+achieveradarsh@users.noreply.github.com>
2024-08-10 11:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,32 +38,37 @@ def get_disease_info():
     data = request.json
     symptoms = data.get('symptoms')
     
-    # Query to find diseases based on symptoms
-    cursor.execute("""
-        SELECT d.disease_id, d.disease_name 
-        FROM diseases d 
-        JOIN symptoms s ON d.disease_id = s.disease_id 
-        WHERE s.symptom_name ILIKE %s
-    """, ('%' + symptoms + '%',))
-    
-    diseases = cursor.fetchall()
-    
-    if not diseases:
-        return jsonify({"error": "No diseases found for the given symptoms."}), 404
-    
-    # Get treatments for each disease
-    disease_info = []
-    for disease in diseases:
-        cursor.execute("SELECT * FROM treatments WHERE disease_id = %s", (disease[0],))
-        treatments = cursor.fetchall()
+    try:
+        # Query to find diseases based on symptoms
+        cursor.execute("""
+            SELECT d.disease_id, d.disease_name 
+            FROM diseases d 
+            JOIN symptoms s ON d.disease_id = s.disease_id 
+            WHERE s.symptom_name ILIKE %s
+        """, ('%' + symptoms + '%',))
         
-        disease_info.append({
-            "disease_id": disease[0],
-            "disease_name": disease[1],
-            "treatments": [{"type": t[2], "description": t[3]} for t in treatments]
-        })
-    
-    return jsonify({"result": disease_info})
+        diseases = cursor.fetchall()
+        
+        if not diseases:
+            return jsonify({"error": "No diseases found for the given symptoms."}), 404
+        
+        # Get treatments for each disease
+        disease_info = []
+        for disease in diseases:
+            cursor.execute("SELECT * FROM treatments WHERE disease_id = %s", (disease[0],))
+            treatments = cursor.fetchall()
+            
+            disease_info.append({
+                "disease_id": disease[0],
+                "disease_name": disease[1],
+                "treatments": [{"type": t[2], "description": t[3]} for t in treatments]
+            })
+        
+        return jsonify({"result": disease_info})
+
+    except Exception as e:
+        conn.rollback()  # Rollback in case of error
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
